e31f9488572aacc4cf59958dd39aed81b26cf3a1
Marine FRANCESCHI <franceschi.marine@gmail.com>
2017-07-02 19:22:53 +0200

    conso by type

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
     this.state = {
       price : 0,
-      isHappyHour : false
+      isHappyHour : false,
+      displayedList : null,
     };
     this.addConso = this.addConso.bind(this);
   }
@@ -17,33 +18,53 @@ class App extends Component {
     let currentValue = this.state.isHappyHour;
     this.setState({isHappyHour : !currentValue});
   }
+  toggleList (consoType) {
+    if (this.state.displayedList !== consoType){
+      this.setState({displayedList : consoType});
+    }
+    else {
+      this.setState({displayedList : null});
+    }
+  }
   addConso (conso) {
     let currentPrice = this.state.price;
     let priceToAdd = this.state.isHappyHour && conso.happyHourPrice ? conso.happyHourPrice : conso.price;
     let newPrice = currentPrice + priceToAdd;
-    this.setState({price : newPrice});
+    this.setState({price : newPrice, displayedList: null});
   }
   reset(){
-    this.setState({price:0});
+    this.setState({price:0, displayedList: null});
   }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Celtic-Conso</h2>
         </div>
         <p className="App-intro">
           You must currently pay {this.state.price.toFixed(2)} €
         </p>
         <input onClick ={this.toggleHappyHour.bind(this)} type ="checkbox"/> Happy Hour
-        {consos.map((conso)=>{
-          return <button key={conso.name} onClick={()=>this.addConso(conso)}>{conso.name}</button>
-        })};
-        <button onClick={this.reset.bind(this)}>Reset</button>
+        {Object.keys(consos).map(displayConsoList.bind(this))}
+        <button className="type" onClick={this.reset.bind(this)}>Reset</button>
       </div>
     );
   }
 }
+function displayConsoList(consoType) {
+  return (
+    <div key={consoType}>
+      <button className="type" onClick={() => this.toggleList(consoType)}>{consoType}</button>
+      {
+        this.state.displayedList === consoType &&
+        consos[consoType].map((conso) =>
+          <button key={conso.name} onClick={() => this.addConso(conso)}>{conso.name} : {conso.price.toFixed(2)} €</button>
+        )
+      }
+    </div>
+  )
+}
+
 
 export default App;
--- src/consos.js
+++ src/consos.js
@@ -1,34 +1,141 @@
-const consos = [
-  {
-    name : "Beer",
-    price : 7.40,
-    happyHourPrice : 5.5
-  },
-  {
-    name : "Cocktail",
-    price : 8
-  },
-  {
-    name : "Jumbo Cocktail",
-    price : 14
-  },
-  {
-    name : "Shot",
-    price : 4
-  },
-  {
-    name : "Soft",
-    price : 4,
-    happyHourPrice : 3,
-  },
-  {
-    name : "Tapas",
-    price : 7.5
-  },
-  {
-    name : "Food",
-    price : 15
-  },
-];
+const consos = {
+  Beers : [
+    {
+      name : "Affligem",
+      price : 7.40,
+      happyHourPrice : 5.50
+    },
+    {
+      name : "Pelforth",
+      price : 6.90,
+      happyHourPrice : 5.50
+    },
+    {
+      name : "Cidre",
+      price : 7.40,
+      happyHourPrice : 5.50
+    },
+    {
+      name : "Guiness",
+      price : 7.40,
+    },
+    {
+      name : "Bottles",
+      price : 6.50,
+    },
+  ],
+
+  Cocktails : [
+    {
+      name : "Tequila Sunrise",
+      price : 8,
+    },
+    {
+      name : "Sex on the Beach",
+      price : 8,
+    },
+    {
+      name : "Cosmopolitan",
+      price : 8,
+    },
+    {
+      name : "Ti-punch",
+      price : 8,
+    },
+  ],
+
+  Whiskies : [
+    {
+      name : "Jameson",
+      price : 7,
+    },
+    {
+      name : "Bushmills",
+      price : 9,
+    },
+    {
+      name : "Jack Daniels",
+      price : 10,
+    },
+    {
+      name : "Chivas",
+      price : 12,
+    },
+    {
+      name : "Laphroaig",
+      price : 12,
+    },
+  ],
+
+  Shots : [
+    {
+      name : "Basique",
+      price : 4,
+    },
+    {
+      name : "Jägermeister",
+      price : 4,
+    },
+    {
+      name : "Jägerbomb",
+      price : 6,
+    },
+    {
+      name : "Arrangée",
+      price : 4,
+    },
+  ],
+
+  Softs : [
+    {
+      name : "Basique",
+      price : 4,
+      happyHourPrice : 3,
+    },
+    {
+      name : "RedBull",
+      price : 5,
+    },
+  ],
+
+  Tapas : [
+    {
+      name : "Saucisson",
+      price : 7,
+    },
+    {
+      name : "Sticks",
+      price : 6.50,
+    },
+    {
+      name : "Chips",
+      price : 1.50,
+    },
+    {
+      name : "Big Tapas",
+      price : 15,
+    },
+  ],
+
+  Food : [
+    {
+      name : "Burger",
+      price : 15,
+    },
+    {
+      name : "Salade",
+      price : 11,
+    },
+    {
+      name : "Hot Dog",
+      price : 8.50,
+    },
+    {
+      name : "Fish & Chips",
+      price : 12,
+    },
+  ],
+};
+
 
 export default consos;
