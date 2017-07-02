bf528059583f0a872ff70f1c1f2076389ef03028
Marine FRANCESCHI <franceschi.marine@gmail.com>
2017-07-02 17:30:18 +0200

    refactor in array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './beer.png';
 import './App.css';
+import consos from './consos.js';
+
 
 class App extends Component {
   constructor(){
@@ -9,45 +11,20 @@ class App extends Component {
       price : 0,
       isHappyHour : false
     };
+    this.addConso = this.addConso.bind(this);
   }
   toggleHappyHour () {
     let currentValue = this.state.isHappyHour;
-    this.setState({isHappyHour : !currentValue})
+    this.setState({isHappyHour : !currentValue});
   }
-  addBeer () {
-    let beerPrice = this.state.isHappyHour ? 5.50 : 7.40;
+  addConso (conso) {
     let currentPrice = this.state.price;
-    let newPrice = currentPrice + beerPrice;
-    this.setState({price : newPrice})
-  };
-  addCocktail(){
-    let currentPrice = this.state.price;
-    let newPrice = currentPrice + 8;
-    this.setState({price : newPrice})
-  }
-  addJumboCocktail(){
-    let currentPrice = this.state.price;
-    let newPrice = currentPrice + 14;
-    this.setState({price : newPrice})
-  }
-  addShot(){
-    let currentPrice = this.state.price;
-    let newPrice = currentPrice + 4;
-    this.setState({price : newPrice})
-  }
-
-  addTapas(){
-    let currentPrice = this.state.price;
-    let newPrice = currentPrice + 7.50;
-    this.setState({price : newPrice})
-  }
-  addFood(){
-    let currentPrice = this.state.price;
-    let newPrice = currentPrice + 15;
-    this.setState({price: newPrice})
+    let priceToAdd = this.state.isHappyHour && conso.happyHourPrice ? conso.happyHourPrice : conso.price;
+    let newPrice = currentPrice + priceToAdd;
+    this.setState({price : newPrice});
   }
   reset(){
-    this.setState({price:0})
+    this.setState({price:0});
   }
   render() {
     return (
@@ -60,14 +37,10 @@ class App extends Component {
           You must currently pay {this.state.price.toFixed(2)} €
         </p>
         <input onClick ={this.toggleHappyHour.bind(this)} type ="checkbox"/> Happy Hour
-        <button onClick ={this.addBeer.bind(this)}>Beer</button>
-        <br/><button onClick={this.addCocktail.bind(this)}>Cocktail</button>
-        <br/><button onClick={this.addJumboCocktail.bind(this)}>Jumbo Cocktail</button>
-        <br/><button onClick={this.addShot.bind(this)}>Shot</button>
-        <br/><button onClick ={this.addTapas.bind(this)}>Tapas</button>
-        <br/><button onClick ={this.addFood.bind(this)}>Food</button>
-        <br/><button onClick ={this.reset.bind(this)}>Reset</button>
-
+        {consos.map((conso)=>{
+          return <button key={conso.name} onClick={()=>this.addConso(conso)}>{conso.name}</button>
+        })};
+        <button onClick={this.reset.bind(this)}>Reset</button>
       </div>
     );
   }
---
+++ src/consos.js
@@ -0,0 +1,34 @@
+const consos = [
+  {
+    name : "Beer",
+    price : 7.40,
+    happyHourPrice : 5.5
+  },
+  {
+    name : "Cocktail",
+    price : 8
+  },
+  {
+    name : "Jumbo Cocktail",
+    price : 14
+  },
+  {
+    name : "Shot",
+    price : 4
+  },
+  {
+    name : "Soft",
+    price : 4,
+    happyHourPrice : 3,
+  },
+  {
+    name : "Tapas",
+    price : 7.5
+  },
+  {
+    name : "Food",
+    price : 15
+  },
+];
+
+export default consos;
